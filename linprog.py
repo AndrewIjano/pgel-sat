6002e772b65e655e15d9ab1fc7d9c45f6cd212a6
Andrew Ijano <andrew.lopes@uol.com.br>
2020-03-17 08:04:04 -0300

    Refactors linprog.py code

--- linprog.py
+++ linprog.py
@@ -1,5 +1,5 @@
 import numpy as np
-from swiglpk import *
+import swiglpk as glpk
 
 
 def solve(c, C, d):
@@ -9,7 +9,7 @@ def solve(c, C, d):
     cols_count = set_objective(lp, c)
     set_coefficients(lp, C)
 
-    glp_interior(lp, None)
+    optimize(lp)
 
     x = get_primal_solution(lp, cols_count)
     y = get_dual_solution(lp, rows_count)
@@ -20,55 +20,61 @@ def solve(c, C, d):
 
 
 def create_minimization_problem():
-    glp_term_out(GLP_OFF)
+    glpk.glp_term_out(glpk.GLP_OFF)
 
-    lp = glp_create_prob()
-    glp_set_obj_dir(lp, GLP_MIN)
+    lp = glpk.glp_create_prob()
+    glpk.glp_set_obj_dir(lp, glpk.GLP_MIN)
     return lp
 
 
 def set_rows(lp, d):
     rows_count = len(d)
-    glp_add_rows(lp, rows_count)
+    glpk.glp_add_rows(lp, rows_count)
     for idx, bnd in enumerate(d):
-        glp_set_row_bnds(lp, idx + 1, GLP_FX, bnd, bnd)
+        glpk.glp_set_row_bnds(lp, idx + 1, glpk.GLP_FX, bnd, bnd)
 
     return rows_count
 
 
 def set_objective(lp, c):
     cols_count = len(c)
-    glp_add_cols(lp, cols_count)
+    glpk.glp_add_cols(lp, cols_count)
     for idx, coef in enumerate(c):
-        glp_set_col_bnds(lp, idx + 1, GLP_LO, 0.0, 0.0)
-        glp_set_obj_coef(lp, idx + 1, coef)
+        glpk.glp_set_col_bnds(lp, idx + 1, glpk.GLP_LO, 0.0, 0.0)
+        glpk.glp_set_obj_coef(lp, idx + 1, coef)
 
     return cols_count
 
 
 def set_coefficients(lp, C):
     elements_count = np.count_nonzero(C)
-    i_C = intArray(1 + elements_count)
-    j_C = intArray(1 + elements_count)
-    a_C = doubleArray(1 + elements_count)
+    i_C = glpk.intArray(1 + elements_count)
+    j_C = glpk.intArray(1 + elements_count)
+    a_C = glpk.doubleArray(1 + elements_count)
 
     for idx, (i, j) in enumerate(zip(*C.nonzero()), 1):
         i_C[idx] = int(i + 1)
         j_C[idx] = int(j + 1)
         a_C[idx] = float(C[i, j])
 
-    glp_load_matrix(lp, elements_count, i_C, j_C, a_C)
+    glpk.glp_load_matrix(lp, elements_count, i_C, j_C, a_C)
+
+
+def optimize(lp):
+    glpk.glp_interior(lp, None)
 
 
 def get_primal_solution(lp, cols_count):
-    return [glp_ipt_col_prim(lp, i + 1) for i in range(cols_count)]
+    return [glpk.glp_ipt_col_prim(lp, i + 1) for i in range(cols_count)]
 
 
 def get_dual_solution(lp, rows_count):
-    return [glp_ipt_row_dual(lp, j + 1) for j in range(rows_count)]
+    return [glpk.glp_ipt_row_dual(lp, j + 1) for j in range(rows_count)]
+
 
 def get_cost(lp):
-    return glp_ipt_obj_val(lp)
+    return glpk.glp_ipt_obj_val(lp)
+
 
 def delete_problem(lp):
-    glp_delete_prob(lp)
+    glpk.glp_delete_prob(lp)
